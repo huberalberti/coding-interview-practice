1e28772ec2cb57cf5c074a641a5b7e3c38e7906d
huberalberti <huberalberti@gmail.com>
2022-02-08 10:12:40 -0300

    Add SquareOfSortedArray.kt

---
+++ src/main/kotlin/leetCode/SquareOfSortedArray.kt
@@ -0,0 +1,31 @@
+package leetCode
+//Given an integer array nums sorted in non-decreasing order, return an array of the squares
+// of each number sorted in non-decreasing order.
+
+
+// - Iterate over nums, generate squares and then sort. O(nlogn)
+// - Iterate over nums, generate squares and add into sorted array O(nlogn)
+// - Two pointers O(n)
+
+class SquareOfSortedArray {
+    operator fun invoke(nums: IntArray): IntArray{
+        var left = 0
+        var right = nums.size - 1
+        var result = mutableListOf<Int>()
+
+        while(left <= right){
+            val squareLeft = nums[left] * nums [left]
+            val squareRight = nums[right] * nums [right]
+
+            if(squareLeft >= squareRight){
+                result.add(0,squareLeft)
+                left ++
+            }
+            else{
+                result.add(0, squareRight)
+                right --
+            }
+        }
+        return result.toIntArray()
+    }
+}
